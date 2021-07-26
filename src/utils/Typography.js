4136f05df9a463a0d3b5f390e933608265632c1d
Deependra Nyati <deependra.nyati@xebia.com>
2021-07-26 18:10:31 +0530

    Typography

---
+++ src/utils/Typography.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Typography = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Text>Help Patient</Text>
+    </View>
+  );
+};
+
+export default Typography;
